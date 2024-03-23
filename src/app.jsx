a260691533872871163bbde2716a6e314e139284
Gabriel Moraes <121532661+angrycsm@users.noreply.github.com>
2024-03-23 22:26:09 +0000

    Updating Header Component

--- src/app.jsx
+++ src/app.jsx
@@ -15,6 +15,8 @@ export default function App() {
 			<Header/>
   	</div>
 		</header>
+    
+		
 			  
   )
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,11 @@
 export function Header() {
+	const textDecoration = "hover:text-pink-400 font-semibold transition-colors duration-800"
 	return(
 		<>
 			<div className="w-full flex p-4 m-4 justify-end space-x-3.5">
-				<a href="#" className="hover:text-pink-400 font-semibold transition-colors duration-800">Início</a>
-				<a href="#" className="hover:text-pink-400 font-semibold transition-colors duration-800">Sobre</a>
-				<a href="#" className="hover:text-pink-400 font-semibold transition-colors duration-800">Cards</a>
+				<a href="#" className={textDecoration}>Início</a>
+				<a href="#" className={textDecoration}>Sobre</a>
+				<a href="#" className={textDecoration}>Cards</a>
 			</div>
 		</>
 	)
